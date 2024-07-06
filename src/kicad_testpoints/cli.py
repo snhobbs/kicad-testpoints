b5fd6c9737fd4059ad39557dfc0c0d94cb69dd21
simon <simon.hobbs@electrooptical.net>
2024-07-06 14:59:50 -0400

    adding test point by fabrication setting option

--- src/kicad_testpoints/cli.py
+++ src/kicad_testpoints/cli.py
@@ -9,10 +9,19 @@ import pcbnew
 from . import file_io
 from . import kicad_testpoints
 
-_log = None
+_log = logging.getLogger("kicad_testpoints")
 
 
-@click.command(
+@click.option("--debug", is_flag=True, help="")
+@click.version_option()
+@click.group()
+def gr1(debug):
+    _log.setLevel(logging.INFO)
+    if debug:
+        _log.setLevel(logging.DEBUG)
+
+
+@gr1.command(
     help="Takes a PCB & configuration data in mm, sets rotation and location on a new pcb"
 )
 @click.option("--pcb", type=str, required=True, help="Source PCB file")
@@ -24,15 +33,7 @@ _log = None
     "--drill-center", is_flag=True, help="Use drill/file center as reference coordinate"
 )
 @click.option("--inplace", is_flag=True, help="Edit probe spreadsheet inplace")
-@click.option("--debug", is_flag=True, help="")
-@click.version_option()
-def main(pcb, points, out, drill_center, inplace, debug):
-    global _log
-    _log = logging.getLogger()
-    _log.setLevel(logging.WARNING)
-    if debug:
-        _log.setLevel(logging.DEBUG)
-
+def from_spreadsheet(pcb, points, out, drill_center, inplace):
     if inplace:
         out = points
     elif out is None:
@@ -58,6 +59,36 @@ def main(pcb, points, out, drill_center, inplace, debug):
     file_io.write(report_df, out)
 
 
+@gr1.command(
+    help="Pull out the position and net for all pads with a test point property set."
+)
+@click.option("--pcb", type=str, required=True, help="Source PCB file")
+@click.option("--out", type=str, required=True, help="Output spreadsheet")
+@click.option(
+    "--drill-center", is_flag=True, help="Use drill/file center as reference coordinate"
+)
+def by_fab_setting(pcb, out, drill_center):
+    board = pcbnew.LoadBoard(pcb)
+    pads = kicad_testpoints.get_pads_by_property(board)
+
+    if len(pads) == 0:
+        _log.warning("No pads with fabrication setting found")
+        return
+
+    assert len(pads) > 0
+    settings = kicad_testpoints.Settings()
+    settings.use_aux_origin = drill_center
+
+    report = kicad_testpoints.build_test_point_report(board, settings, pads)
+    report_df = pd.DataFrame(report)
+    file_io.write(report_df, out)
+
+
+def main():
+    gr1()
+
+
 if __name__ == "__main__":
     logging.basicConfig()
+    _log.setLevel(logging.INFO)
     main()  # pragma: no cover
